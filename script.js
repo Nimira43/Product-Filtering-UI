971c1aa2acffcc045f6a77b41400c73af791ef57
Nick Rabone <n.rabone72@aol.com>
2024-03-02 20:30:36 +0000

    Tailwind and HTML completed.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const products = [
     {
-      name: 'Sony Playstation 5',
-      url: 'images/playstation_5.png',
+      name: 'Samsung TV',
+      url: 'images/samsung_tv.png',
+      type: 'televisions',
+      price: 1099.99,
+    },
+    {
+      name: 'Nintendo Switch',
+      url: 'images/nintendo_switch.png',
       type: 'games',
-      price: 499.99,
+      price: 299.99,
+    },
+    { 
+      name: 'Sony ZV1F Camera',
+      url: 'images/sony_zv1f_camera.png',
+      type: 'cameras',
+      price: 799.99,
     },
     {
       name: 'Samsung Galaxy',
@@ -12,28 +24,10 @@ const products = [
       price: 399.99,
     },
     {
-      name: 'Cannon EOS Camera',
-      url: 'images/cannon_eos_camera.png',
+      name: 'Canon EOS Camera',
+      url: 'images/canon_eos_camera.png',
       type: 'cameras',
       price: 749.99,
-    },
-    {
-      name: 'Sony A7 Camera',
-      url: 'images/sony_a7_camera.png',
-      type: 'cameras',
-      price: 1999.99,
-    },
-    {
-      name: 'LG TV',
-      url: 'images/lg_tv.png',
-      type: 'televisions',
-      price: 799.99,
-    },
-    {
-      name: 'Nintendo Switch',
-      url: 'images/nintendo_switch.png',
-      type: 'games',
-      price: 299.99,
     },
     {
       name: 'Xbox Series X',
@@ -42,11 +36,18 @@ const products = [
       price: 499.99,
     },
     {
-      name: 'Samsung TV',
-      url: 'images/samsung_tv.png',
+      name: 'LG TV',
+      url: 'images/lg_tv.png',
       type: 'televisions',
-      price: 1099.99,
+      price: 799.99,
     },
+    {
+      name: 'Sony Playstation 5',
+      url: 'images/playstation_5.png',
+      type: 'games',
+      price: 499.99,
+    },
+    
     {
       name: 'Google Pixel',
       url: 'images/google_pixel.png',
@@ -54,16 +55,16 @@ const products = [
       price: 499.99,
     },
     {
-      name: 'Sony ZV1F Camera',
-      url: 'images/sony_zv1f_camera.png',
-      type: 'cameras',
-      price: 799.99,
-    },
-    {
       name: 'Toshiba TV',
       url: 'images/toshiba_tv.png',
       type: 'televisions',
       price: 499.99,
+    },
+    {
+      name: 'Sony A7 Camera',
+      url: 'images/sony_a7_camera.png',
+      type: 'cameras',
+      price: 1999.99,
     },
     {
       name: 'iPhone 14',
