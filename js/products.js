74beded71428b7fac223c4e2752963a8da53c0e8
Nick Rabone <n.rabone72@aol.com>
2024-07-23 10:31:12 +0100

    Put products array into its on file

---
+++ js/products.js
@@ -0,0 +1,75 @@
+export const products = [
+  {
+    name: 'Samsung TV',
+    url: 'img/Samsung-TV.png',
+    type: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'img/Nintendo-Switch.png',
+    type: 'games',
+    price: 299.99,
+  },
+  { 
+    name: 'Sony ZV1F Camera',
+    url: 'img/Sony-ZV1F.png',
+    type: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'img/Samsung-Galaxy.png',
+    type: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Canon EOS Camera',
+    url: 'img/Canon-Eos.png',
+    type: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'img/Xbox-Series-X.png',
+    type: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'img/LG-TV.png',
+    type: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Sony Playstation 5',
+    url: 'img/PlayStation-5.png',
+    type: 'games',
+    price: 499.99,
+  },
+  
+  {
+    name: 'Google Pixel',
+    url: 'img/Google-Pixel.png',
+    type: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'img/Toshiba-TV.png',
+    type: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'img/Sony-A7.png',
+    type: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'iPhone 14',
+    url: 'img/iPhone-14.png',
+    type: 'smartphones',
+    price: 999.99,
+  },
+];
