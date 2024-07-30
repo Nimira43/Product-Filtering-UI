ef854222bf373c5b5e0acfdb4ec5b82d69eda223
Nick Rabone <n.rabone72@aol.com>
2024-07-30 22:32:08 +0100

    Brought in DOM element const variables and made correction to HTML

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 import { products } from "./js/products.js"
 
-
+const productsWrapperEl = document.getElementById('products-wrapper')
+const checksEls = document.querySelectorAll('.check')
+const filtersContainer = document.getElementById('filters-container')
+const searchInput = document.getElementById('search')
+const cartButton = document.getElementById('cartButton')
+const cartCount = document.getElementById('cartCount')
 
