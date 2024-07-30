c086f213a08729c881cfb9404ba9c0f7b982afc7
Nick Rabone <n.rabone72@aol.com>
2024-07-30 23:48:10 +0100

    Tidy up

--- script.js
+++ script.js
@@ -25,31 +25,25 @@ function createProductElement(product) {
   productEl.innerHTML = 
     
     `
-      
       <div
         class="bg-[#fff] flex justify-center relative overflow-hidden group cursor-pointer border border-[#ff4500]"
       >
-        
         <img
           src="${product.url}"
           alt="${product.name}"
           class="w-full h-fill object-cover"
         >
-        
         <span
           class="status bg-[#000] text-[#fff] absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition border-t border-[#ff4500] group-hover:translate-y-0"
         >
           Add To Cart
         </span>
-      
       </div>
-      
       <p
         class="text-xl"
       >
         ${product.name}
       </p>
-      
       <strong>
         ${product.price.toLocaleString()}
       </strong>      
